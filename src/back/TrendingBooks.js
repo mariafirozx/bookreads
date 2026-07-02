4c9c81758705f2371fcd2668fef48f46ce66aba5
Maria <mariafiroz9@gmail.com>
2026-07-02 21:38:25 +0300

    book_card changes

---
+++ src/back/TrendingBooks.js
@@ -0,0 +1,89 @@
+import React from "react";
+import {useState, useEffect} from "react";
+
+export default function TrendingBooks(){
+
+      const trending = [
+        {
+            id: 1,
+            img: "/imgs/8.jpg",
+            title: "A Marriage at Sea",
+            author: "Sophie Elmhirst"
+        },
+        {
+            id: 2,
+            img: "/imgs/16.jpg",
+            title: "Welcome to the Hyunam-Dong Bookshop",
+            author: "Hwang Bo-Reum"
+        },
+        {   
+            id:3,
+            img: "/imgs/1.jpg",
+            title: "Finding Grace",
+            author: "Loretta Rothschild"
+
+        },
+        {
+            id: 4,
+            img: "/imgs/14.jpg",
+            title: "The Wedding People",
+            author: "Alison Espach"
+        }, 
+        {
+            id: 5,
+            img: "/imgs/26.jpeg",
+            title: "loren posem",
+            author: "Emily"
+        },
+        {
+            id: 6,
+            img: "/imgs/27.jpg",
+            title: "The Blond Who Came In From The Gold",
+            author: "Ally Carter"
+        },
+        {
+             id: 7,
+            img: "/imgs/35.jpeg",
+            title: "Blood Over Bright Haven",
+            author: "M.L. Wang"
+
+        },
+        {
+             id: 8,
+            img: "/imgs/37.jpeg",
+            title: "Writing for Busy Readers",
+            author: "Todd Rogers & Jessica Lasky-Fink"
+        },
+        {
+             id: 8,
+            img: "/imgs/37.jpeg",
+            title: "Writing for Busy Readers",
+            author: "Todd Rogers & Jessica Lasky-Fink"
+        },
+        {
+             id: 8,
+            img: "/imgs/37.jpeg",
+            title: "Writing for Busy Readers",
+            author: "Todd Rogers & Jessica Lasky-Fink"
+        },
+        {
+             id: 8,
+            img: "/imgs/37.jpeg",
+            title: "Writing for Busy Readers",
+            author: "Todd Rogers & Jessica Lasky-Fink"
+        },
+        {
+             id: 8,
+            img: "/imgs/37.jpeg",
+            title: "Writing for Busy Readers",
+            author: "Todd Rogers & Jessica Lasky-Fink"
+        },
+      
+
+    ]
+
+    return(
+        <></>
+
+    )
+}
--- src/front/Home.js
+++ src/front/Home.js
@@ -105,9 +105,113 @@ export default function Home(){
             img: "/imgs/37.jpeg",
             title: "Writing for Busy Readers",
             author: "Todd Rogers & Jessica Lasky-Fink"
-        }
+        },
+        {
+             id: 8,
+            img: "/imgs/37.jpeg",
+            title: "Writing for Busy Readers",
+            author: "Todd Rogers & Jessica Lasky-Fink"
+        },
+        {
+             id: 8,
+            img: "/imgs/37.jpeg",
+            title: "Writing for Busy Readers",
+            author: "Todd Rogers & Jessica Lasky-Fink"
+        },
+        {
+             id: 8,
+            img: "/imgs/37.jpeg",
+            title: "Writing for Busy Readers",
+            author: "Todd Rogers & Jessica Lasky-Fink"
+        },
+        {
+             id: 8,
+            img: "/imgs/37.jpeg",
+            title: "Writing for Busy Readers",
+            author: "Todd Rogers & Jessica Lasky-Fink"
+        },
+      
+
     ]
 
+    const recommended = [
+        {
+            id: 1,
+            img: "/imgs/8.jpg",
+            title: "A Marriage at Sea",
+            author: "Sophie Elmhirst"
+        },
+        {
+            id: 2,
+            img: "/imgs/16.jpg",
+            title: "Welcome to the Hyunam-Dong Bookshop",
+            author: "Hwang Bo-Reum"
+        },
+        {   
+            id:3,
+            img: "/imgs/1.jpg",
+            title: "Finding Grace",
+            author: "Loretta Rothschild"
+
+        },
+        {
+            id: 4,
+            img: "/imgs/14.jpg",
+            title: "The Wedding People",
+            author: "Alison Espach"
+        }, 
+        {
+            id: 5,
+            img: "/imgs/26.jpeg",
+            title: "loren posem",
+            author: "Emily"
+        },
+        {
+            id: 6,
+            img: "/imgs/27.jpg",
+            title: "The Blond Who Came In From The Gold",
+            author: "Ally Carter"
+        },
+        {
+             id: 7,
+            img: "/imgs/35.jpeg",
+            title: "Blood Over Bright Haven",
+            author: "M.L. Wang"
+
+        },
+        {
+             id: 8,
+            img: "/imgs/37.jpeg",
+            title: "Writing for Busy Readers",
+            author: "Todd Rogers & Jessica Lasky-Fink"
+        },
+        {
+             id: 8,
+            img: "/imgs/37.jpeg",
+            title: "Writing for Busy Readers",
+            author: "Todd Rogers & Jessica Lasky-Fink"
+        },
+        {
+             id: 8,
+            img: "/imgs/37.jpeg",
+            title: "Writing for Busy Readers",
+            author: "Todd Rogers & Jessica Lasky-Fink"
+        },
+        {
+             id: 8,
+            img: "/imgs/37.jpeg",
+            title: "Writing for Busy Readers",
+            author: "Todd Rogers & Jessica Lasky-Fink"
+        },
+        {
+             id: 8,
+            img: "/imgs/37.jpeg",
+            title: "Writing for Busy Readers",
+            author: "Todd Rogers & Jessica Lasky-Fink"
+        },
+      
+
+    ]
     return(
         <>
 
@@ -193,6 +297,19 @@ export default function Home(){
                 <section className="todays_rec">
                     <div className="rb-track">
                         <div className="grid-container-rb">
+                        {
+                            recommended.map((rec, i)=>(
+                                <BookCard
+                                    key={i+1}
+                                    img={rec.img}
+                                    title={rec.title}
+                                    author={rec.author}
+                                    type="recommended"
+                                ></BookCard>
+                            ))
+                        }
+
+
                             
                         </div>
                     </div>
